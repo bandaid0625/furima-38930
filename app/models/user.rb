1a8bba31b698ec1fa52b1441b93a28e58f9c9170
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-06 17:27:34 +0900

    validationやり直し３

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  validates :password, on: :create, format: {with: /A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}z/, message:"半角英数字、6文字以上で入力してください。"}
+  validates :password, on: :create, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message:" is invalid. Include both letters and numbers"}, 
+  length: { minimum: 6, message:" is too short (minimum is 6 characters)"}
   validates :nickname, presence: true       
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters"}
   validates :birth_day, presence: true
 end
