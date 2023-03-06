437cbeddc0df4154b7a26686752d8292523a4854
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-06 19:39:40 +0900

    バリデーション手直し４

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
 
-  validates :password, on: :create, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message:" is invalid. Include both letters and numbers"}, 
-  length: { minimum: 6, message:" is too short (minimum is 6 characters)"}
-  validates :nickname, presence: true       
+  validates :nickname, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message:"is invalid. Include both letters and numbers"}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters"}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters"}
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters"}
