5f6319a8b0f6af20b23d3b780b26e77e8a10ac4c
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-06 16:50:56 +0900

    validation追加２

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
-         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
-         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
-         validates :birth_day, presence: true
+  validates :password, on: :create, format: {with: /A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}z/, message:"半角英数字、6文字以上で入力してください。"}
+  validates :nickname, presence: true       
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"全角（漢字・ひらがな・カタカナ）で入力してください。"}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message:"カタカナで入力してください。"}
+  validates :birth_day, presence: true
 end
