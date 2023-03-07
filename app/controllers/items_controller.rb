cd286f7585035278858fcd86ec8c83256b6dff91
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-07 21:02:21 +0900

    商品出品表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
