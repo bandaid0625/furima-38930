e45ace145031c51219e72c39a7f524e744ed910b
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-08 16:04:18 +0900

    修正　Math.round→Math .floor

--- app/javascript/caluculation.js
+++ app/javascript/caluculation.js
@@ -2,9 +2,9 @@ function calculation () {
   const price = document.getElementById("item-price");
   price.addEventListener("input", () => {
     const addTax = document.getElementById("add-tax-price");
-    addTax.innerHTML = Math.round(price.value * 0.1);
+    addTax.innerHTML = Math.floor(price.value * 0.1);
     const sum = document.getElementById("profit");
-    sum.innerHTML = Math.round(price.value - Math.round(price.value * 0.1));
+    sum.innerHTML = Math.round(price.value - Math.floor(price.value * 0.1));
   });
 };
 
