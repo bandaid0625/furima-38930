dca0199ee3ed958900c03f85b1f2114b557b9fb3
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-08 16:46:35 +0900

    修正

--- app/javascript/caluculation.js
+++ app/javascript/caluculation.js
@@ -4,7 +4,7 @@ function calculation () {
     const addTax = document.getElementById("add-tax-price");
     addTax.innerHTML = Math.floor(price.value * 0.1);
     const sum = document.getElementById("profit");
-    sum.innerHTML = Math.round(price.value - Math.floor(price.value * 0.1));
+    sum.innerHTML = Math.floor(price.value - Math.floor(price.value * 0.1));
   });
 };
 
