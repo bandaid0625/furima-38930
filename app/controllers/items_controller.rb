c2b549d1559c36010c59c81b807324b29b8b1c7e
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-09 19:12:21 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.order(created_at: :DESC)
@@ -23,6 +23,23 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if user_signed_in? && (current_user.id != @item.user_id)
+      redirect_to action: :index
+    end
+    # if ((@item.orders)!= nil) || (current_user.id != @item.user_id)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render action: :edit
+    end
   end
 
   private
