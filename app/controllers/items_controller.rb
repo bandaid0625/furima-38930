24f950e37b2b5b118951a87db8b8917a3a1cab19
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-09 19:27:14 +0900

    修正　同じ記述まとめ、user_signed_in?削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :DESC)
@@ -21,20 +22,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if user_signed_in? && (current_user.id != @item.user_id)
+    if current_user.id != @item.user_id
       redirect_to action: :index
     end
     # if ((@item.orders)!= nil) || (current_user.id != @item.user_id)
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :sheduled_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
