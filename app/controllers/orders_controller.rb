cccb01b592ebb9df7a70ec0d6de1437006a1df53
bandaid0625 <124765960+bandaid0625@users.noreply.github.com>
2023-03-11 19:07:25 +0900

    form オブジェクト

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
+  end
+
+  def new
+    @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :street_num, :building, :tel, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
